5c28ee3a6a1fe84255cc1bdec501b61fcc2c6a1b
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-27 15:17:56 +0000

    Printed all the words from the txt file:
    - replaced some special characters with an empty one

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var WordCount = require('./src/wordCount');
 
 var server = http.createServer(function (req, res) {
 	var count = new WordCount();
+	count.pushWords();
+	console.log(count.words)
 });
 server.listen(8000);
 console.log('The server is running...')
--- src/wordCount.js
+++ src/wordCount.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
+
 function WordCount(){
 	this.words = []
 };
 
+WordCount.prototype.pushWords = function(){
+  var data = fs.readFileSync('./the-railway-children.txt', "utf8");
+  this.words = data.toUpperCase().replace(/_|\.|\'/g, '').split(/\s+/);
+};
 
 module.exports = WordCount;
